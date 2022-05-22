a19ccab168aa296ac009b41026c76e15361ce82b
Cami <jcamilouc1@gmail.com>
2022-05-22 11:33:24 -0500

    Added Navegation

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.melvin.ongandroid.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.melvin.ongandroid.R
 
@@ -8,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            val intent = Intent (this, Navigation::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
