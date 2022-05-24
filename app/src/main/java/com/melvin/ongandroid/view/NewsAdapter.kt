485f013b9c1283134ef76250d46e968c0d512994
Cami <jcamilouc1@gmail.com>
2022-05-24 12:27:06 -0500

    changes

--- app/src/main/java/com/melvin/ongandroid/view/NewsAdapter.kt
+++ app/src/main/java/com/melvin/ongandroid/view/NewsAdapter.kt
@@ -3,45 +3,36 @@ package com.melvin.ongandroid.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.melvin.ongandroid.R
-import com.melvin.ongandroid.databinding.FragmentNewsBinding
 import com.melvin.ongandroid.databinding.NewsViewBinding
+import com.melvin.ongandroid.model.Novedades
 
-class NewsAdapter(private val newsList: List<Novedades>):
+class NewsAdapter(private val newsList: List<Novedades>) :
     RecyclerView.Adapter<NewsAdapter.NovedadesViewHolder>() {
 
-
-    class NovedadesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val binding = NewsViewBinding.bind(itemView)
+    class NovedadesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = NewsViewBinding.bind(itemView)
 
         fun bind(novedades: Novedades) {
             binding.newsImageView.setImageResource(novedades.newImage)
             binding.newsTitle.text = novedades.newName
             binding.newsDescription.text = novedades.newDescription
         }
-
-       // val novedadImageview: ImageView = itemView.findViewById(R.id.newsImageView)
-       // val novedadTextViewTitle: TextView = itemView.findViewById(R.id.newsTitle)
-       // val novedadTextViewDescription: TextView = itemView.findViewById(R.id.newsDescription)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NovedadesViewHolder {
-
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_news, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.news_view, parent, false)
         return NovedadesViewHolder(view)
     }
-    override fun onBindViewHolder(holder: NovedadesViewHolder, position: Int) {
 
+    override fun onBindViewHolder(holder: NovedadesViewHolder, position: Int) {
         val novedades = newsList[position]
         holder.bind(novedades)
-
     }
-    override fun getItemCount(): Int {
 
+    override fun getItemCount(): Int {
         return newsList.size
     }
 
