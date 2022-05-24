fa23d6ae2df1f2816e91cc0ceac0762451fe72a9
Matias Rostan <mrostan21@gmail.com>
2022-05-24 10:31:44 -0300

    testimonials endpoint added

--- app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
+++ app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
@@ -1,11 +1,17 @@
 package com.melvin.ongandroid.businesslogic.repository
 
-import com.melvin.ongandroid.model.APIServiceSlide
+import com.melvin.ongandroid.model.APIServices
 import com.melvin.ongandroid.model.data.HomeSlides
+import com.melvin.ongandroid.model.data.Testimonials
 
-class HomeRepository(private val APIService: APIServiceSlide) {
+class HomeRepository(private val APIService: APIServices) {
 
     suspend fun getHomeSlides() : HomeSlides{
         return APIService.getHomeSlides()
+    }
+
+    //Calls APIservice getTestimonials suspend function
+    suspend fun getTestimonials(): Testimonials{
+        return APIService.getTestimonials()
     }
 }
---
+++ app/src/main/java/com/melvin/ongandroid/model/APIServices.kt
@@ -0,0 +1,18 @@
+package com.melvin.ongandroid.model
+
+import com.melvin.ongandroid.model.data.HomeSlides
+import com.melvin.ongandroid.model.data.Testimonials
+import retrofit2.http.GET
+
+interface APIServices {
+
+    @GET("api/slides")
+    suspend fun getHomeSlides(): HomeSlides
+
+
+    //This endpoint returns a list of Testimony
+    @GET("api/testimonials")
+    suspend fun getTestimonials(): Testimonials
+
+
+}
