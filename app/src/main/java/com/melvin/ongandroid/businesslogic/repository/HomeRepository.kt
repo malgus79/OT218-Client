28b13eddc2de281842b80308e41578e7e139818f
Gustavo Maldonado <gamcba7982@gmail.com>
2022-05-26 14:11:54 -0300

    Testimonials layout created

--- app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
+++ app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
@@ -1,9 +1,24 @@
 package com.melvin.ongandroid.businesslogic.repository
 
+<<<<<<< HEAD
 import com.melvin.ongandroid.model.data.news.NewsList
 import com.melvin.ongandroid.model.data.slides.SlidesList
 import com.melvin.ongandroid.model.data.testimonials.TestimonialsList
 import com.melvin.ongandroid.model.network.APIServices
+=======
+<<<<<<< HEAD
+import com.melvin.ongandroid.model.network.APIServices
+=======
+import com.melvin.ongandroid.model.APIServices
+<<<<<<< HEAD
+>>>>>>> e7b752f (Testimonials layout created)
+import com.melvin.ongandroid.model.data.slides.SlidesList
+import com.melvin.ongandroid.model.data.testimonials.TestimonialsList
+=======
+import com.melvin.ongandroid.model.data.HomeSlides
+import com.melvin.ongandroid.model.data.Testimonials
+>>>>>>> 827da8a (Testimonials layout created)
+>>>>>>> a49bb72 (Testimonials layout created)
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,22 +26,37 @@ import javax.inject.Singleton
 class HomeRepository @Inject constructor(private val APIService: APIServices) {
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     //Calls APIService getHomeSlides suspend function
     suspend fun getHomeSlides(): SlidesList {
 =======
+=======
+>>>>>>> a49bb72 (Testimonials layout created)
 <<<<<<< HEAD
  //Calls APIservice getHomeSlides suspend function
     suspend fun getHomeSlides() : SlidesList {
 =======
     //Search in the repository for the API call
+=======
+    //Calls APIservice getHomeSlides suspend function
+>>>>>>> 827da8a (Testimonials layout created)
     suspend fun getHomeSlides(): HomeSlides {
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
 >>>>>>> f744c22 (Hilt dependencies and spinner  added)
         return APIService.getHomeSlides()
     }
 
+<<<<<<< HEAD
     //Calls APIService getTestimonials function
+=======
+<<<<<<< HEAD
+    //Calls APIservice getTestimonials function
+>>>>>>> a49bb72 (Testimonials layout created)
     suspend fun getTestimonials(): TestimonialsList {
+=======
+    //Calls APIservice getTestimonials suspend function
+    suspend fun getTestimonials(): Testimonials {
+>>>>>>> 827da8a (Testimonials layout created)
         return APIService.getTestimonials()
     }
 
--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -14,10 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 <<<<<<< HEAD
+<<<<<<< HEAD
     private val viewModel by viewModels<MainActivityViewModel>()
 =======
     private lateinit var mainActivityViewModel: MainActivityViewModel
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+    private val viewModel by viewModels<MainActivityViewModel>()
+>>>>>>> 827da8a (Testimonials layout created)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         //Show Spinner Loading
 <<<<<<< HEAD
+<<<<<<< HEAD
         viewModel.isShowProgress()
 =======
         mainActivityViewModel.isShowProgress()
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+        viewModel.isShowProgress()
+>>>>>>> 827da8a (Testimonials layout created)
 
         binding.button.setOnClickListener {
             startActivity(Intent(this, NavigationActivity::class.java))
