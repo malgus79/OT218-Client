e7b752f97af36b088f250421e5c1dd53270e0b2a
Gustavo Maldonado <gamcba7982@gmail.com>
2022-05-26 14:11:54 -0300

    Testimonials layout created

--- app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
+++ app/src/main/java/com/melvin/ongandroid/businesslogic/repository/HomeRepository.kt
@@ -1,8 +1,13 @@
 package com.melvin.ongandroid.businesslogic.repository
 
 import com.melvin.ongandroid.model.APIServices
+<<<<<<< HEAD
 import com.melvin.ongandroid.model.data.slides.SlidesList
 import com.melvin.ongandroid.model.data.testimonials.TestimonialsList
+=======
+import com.melvin.ongandroid.model.data.HomeSlides
+import com.melvin.ongandroid.model.data.Testimonials
+>>>>>>> 827da8a (Testimonials layout created)
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,17 +15,26 @@ import javax.inject.Singleton
 class HomeRepository @Inject constructor(private val APIService: APIServices) {
 
 <<<<<<< HEAD
+<<<<<<< HEAD
  //Calls APIservice getHomeSlides suspend function
     suspend fun getHomeSlides() : SlidesList {
 =======
     //Search in the repository for the API call
+=======
+    //Calls APIservice getHomeSlides suspend function
+>>>>>>> 827da8a (Testimonials layout created)
     suspend fun getHomeSlides(): HomeSlides {
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
         return APIService.getHomeSlides()
     }
 
+<<<<<<< HEAD
     //Calls APIservice getTestimonials function
     suspend fun getTestimonials(): TestimonialsList {
+=======
+    //Calls APIservice getTestimonials suspend function
+    suspend fun getTestimonials(): Testimonials {
+>>>>>>> 827da8a (Testimonials layout created)
         return APIService.getTestimonials()
     }
 
