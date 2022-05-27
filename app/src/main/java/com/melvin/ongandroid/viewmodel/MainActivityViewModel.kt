03694eed7602d41ec561ae4a8e09e66154133f1b
Gustavo Maldonado <gamcba7982@gmail.com>
2022-05-27 13:39:31 -0300

    fix commit

--- app/src/main/java/com/melvin/ongandroid/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/MainActivityViewModel.kt
@@ -5,21 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.businesslogic.repository.HomeRepository
-<<<<<<< HEAD
-import com.melvin.ongandroid.model.data.HomeSlides
-=======
->>>>>>> 1fea821a1435f5ab7446eed2c9455d553dc96039
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-<<<<<<< HEAD
-class MainActivityViewModel @Inject constructor (private val homeRepository: HomeRepository) : ViewModel() {
-=======
 class MainActivityViewModel @Inject constructor(private val homeRepository: HomeRepository) :
     ViewModel() {
->>>>>>> 1fea821a1435f5ab7446eed2c9455d553dc96039
 
     //Create states
     private val _state = MutableLiveData<State>()
@@ -44,26 +36,14 @@ class MainActivityViewModel @Inject constructor(private val homeRepository: Home
         }
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    suspend fun getTestimonials(){
-=======
-    //Definition of states
     suspend fun getTestimonials() {
->>>>>>> 1fea821a1435f5ab7446eed2c9455d553dc96039
         _state.value = State.Loading()
         viewModelScope.launch {
             val testimonials = homeRepository.getTestimonials()
         }
     }
 
-<<<<<<< HEAD
-=======
     //Definition of states
->>>>>>> f49350f (Hilt dependencies and spinner  added)
-=======
-    //Definition of states
->>>>>>> 1fea821a1435f5ab7446eed2c9455d553dc96039
     sealed class State() {
         class Success() : State()
         class Failure(val cause: Throwable) : State()
