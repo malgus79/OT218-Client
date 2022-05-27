0d99c604eb31378d458c30d4cdc11689438191c8
Gustavo Maldonado <gamcba7982@gmail.com>
2022-05-27 13:39:31 -0300

    fix commit

--- app/src/main/java/com/melvin/ongandroid/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/MainActivityViewModel.kt
@@ -6,20 +6,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.melvin.ongandroid.businesslogic.repository.HomeRepository
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import com.melvin.ongandroid.model.data.HomeSlides
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+>>>>>>> 03694ee (fix commit)
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 <<<<<<< HEAD
+<<<<<<< HEAD
 class MainActivityViewModel @Inject constructor(private val homeRepository: HomeRepository) :
     ViewModel() {
 =======
 class MainActivityViewModel @Inject constructor (private val homeRepository: HomeRepository) : ViewModel() {
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+class MainActivityViewModel @Inject constructor(private val homeRepository: HomeRepository) :
+    ViewModel() {
+>>>>>>> 03694ee (fix commit)
 
     //Create states
     private val _state = MutableLiveData<State>()
@@ -45,12 +53,16 @@ class MainActivityViewModel @Inject constructor (private val homeRepository: Hom
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     //Definition of states
     suspend fun getTestimonials() {
 =======
 <<<<<<< HEAD
     suspend fun getTestimonials(){
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+    suspend fun getTestimonials() {
+>>>>>>> 03694ee (fix commit)
         _state.value = State.Loading()
         viewModelScope.launch {
             val testimonials = homeRepository.getTestimonials()
@@ -58,7 +70,10 @@ class MainActivityViewModel @Inject constructor (private val homeRepository: Hom
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 737af41 (fix commit)
 <<<<<<< HEAD
     //Definition of states
 =======
@@ -66,6 +81,9 @@ class MainActivityViewModel @Inject constructor (private val homeRepository: Hom
     //Definition of states
 >>>>>>> f49350f (Hilt dependencies and spinner  added)
 >>>>>>> bb72910 (Hilt dependencies and spinner  added)
+=======
+    //Definition of states
+>>>>>>> 03694ee (fix commit)
     sealed class State() {
         class Success() : State()
         class Failure(val cause: Throwable) : State()
