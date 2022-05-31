b9decb7f61491b12c6e228df2dd650c68154218a
Cami <jcamilouc1@gmail.com>
2022-05-30 20:46:08 -0500

    fixes

--- app/src/main/java/com/melvin/ongandroid/model/network/APIServices.kt
+++ app/src/main/java/com/melvin/ongandroid/model/network/APIServices.kt
@@ -3,7 +3,6 @@ package com.melvin.ongandroid.model.network
 import com.melvin.ongandroid.model.data.news.NewsList
 import com.melvin.ongandroid.model.data.slides.SlidesList
 import com.melvin.ongandroid.model.data.testimonials.TestimonialsList
-import com.melvin.ongandroid.model.data.news.NewsList
 
 import retrofit2.http.GET
 
