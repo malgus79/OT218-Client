ac8a857c00e2f6b6f4cb25269bfd25c361cbd5dd
Gustavo Maldonado <gamcba7982@gmail.com>
2022-06-02 12:03:11 -0300

    Add navigation to fragments

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.melvin.ongandroid.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.melvin.ongandroid.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.ActivityNavigationBinding
+import com.melvin.ongandroid.view.fragment.ContactFragment
+import com.melvin.ongandroid.view.fragment.HomeFragment
+import com.melvin.ongandroid.view.fragment.OurActivitiesFragment
+import com.melvin.ongandroid.view.fragment.StaffFragment
 import com.melvin.ongandroid.viewmodel.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNavigationBinding
     private val viewModel by viewModels<MainActivityViewModel>()
+    private lateinit var activeFragment: Fragment
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,69 @@ class MainActivity : AppCompatActivity() {
         //Show Spinner Loading
         viewModel.isShowProgress()
 
+        //Add the fragments to the nav menu
+        setupBottomNav()
 
+    }
 
+    private fun setupBottomNav() {
+        //Initialize Fragment Manager
+        fragmentManager = supportFragmentManager
+
+        //Instantiate fragments
+        val homeFragment = HomeFragment()
+        val staffFragment = StaffFragment()
+        val ourActivitiesFragment = OurActivitiesFragment()
+        val contactFragment = ContactFragment()
+
+        //Initialize Fragment Home
+        activeFragment = homeFragment
+
+        //Add fragment Contact
+        fragmentManager.beginTransaction()
+            .add(R.id.navHostFragment, contactFragment, ContactFragment::class.java.name)
+            .hide(contactFragment).commit()
+
+        //Add fragment OurActivities
+        fragmentManager.beginTransaction()
+            .add(R.id.navHostFragment, ourActivitiesFragment, OurActivitiesFragment::class.java.name)
+            .hide(ourActivitiesFragment).commit()
+
+        //Add fragment Staff
+        fragmentManager.beginTransaction()
+            .add(R.id.navHostFragment, staffFragment, StaffFragment::class.java.name)
+            .hide(staffFragment).commit()
+
+        //Add fragment Home
+        fragmentManager.beginTransaction()
+            .add(R.id.navHostFragment, homeFragment, HomeFragment::class.java.name)
+            .commit()
+
+        //Navigate to the different fragments
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit()
+                    activeFragment = homeFragment
+                    true
+                }
+                R.id.nav_staff -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(staffFragment).commit()
+                    activeFragment = staffFragment
+                    true
+                }
+                R.id.nav_our_activities -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(ourActivitiesFragment).commit()
+                    activeFragment = ourActivitiesFragment
+                    true
+                }
+                R.id.nav_contact -> {
+                    fragmentManager.beginTransaction().hide(activeFragment).show(contactFragment).commit()
+                    activeFragment = contactFragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
