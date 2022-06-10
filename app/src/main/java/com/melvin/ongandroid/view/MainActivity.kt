6087102ddd5336103639229891d6c36e8de04963
Cami <jcamilouc1@gmail.com>
2022-06-10 15:20:31 -0500

    bottom menu navigation fix

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -40,4 +40,66 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.bottom_menu, menu)
         return true
     }
+
+//    Este codigo no es necesario, pues ya en navigation activity se hace con muchas menos lineas de codigo
+//    private fun setupBottomNav() {
+//        //Initialize Fragment Manager
+//        fragmentManager = supportFragmentManager
+//
+//        //Instantiate fragments
+//        val homeFragment = HomeFragment()
+//        val membersFragment = MembersFragment()
+//        val ourActivitiesFragment = OurActivitiesFragment()
+//        val contactFragment = ContactFragment()
+//
+//        //Initialize Fragment Home
+//        activeFragment = homeFragment
+//
+//        //Add fragment Contact
+//        fragmentManager.beginTransaction()
+//            .add(R.id.navHostFragment, contactFragment, ContactFragment::class.java.name)
+//            .hide(contactFragment).commit()
+//
+//        //Add fragment OurActivities
+//        fragmentManager.beginTransaction()
+//            .add(R.id.navHostFragment, ourActivitiesFragment, OurActivitiesFragment::class.java.name)
+//            .hide(ourActivitiesFragment).commit()
+//
+//        //Add fragment Staff
+//        fragmentManager.beginTransaction()
+//            .add(R.id.navHostFragment, membersFragment, MembersFragment::class.java.name)
+//            .hide(membersFragment).commit()
+//
+//        //Add fragment Home
+//        fragmentManager.beginTransaction()
+//            .add(R.id.navHostFragment, homeFragment, HomeFragment::class.java.name)
+//            .commit()
+//
+//        //Navigate to the different fragments
+//        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.nav_home -> {
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit()
+//                    activeFragment = homeFragment
+//                    true
+//                }
+//                R.id.nav_members -> {
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(membersFragment).commit()
+//                    activeFragment = membersFragment
+//                    true
+//                }
+//                R.id.nav_our_activities -> {
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(ourActivitiesFragment).commit()
+//                    activeFragment = ourActivitiesFragment
+//                    true
+//                }
+//                R.id.nav_contact -> {
+//                    fragmentManager.beginTransaction().hide(activeFragment).show(contactFragment).commit()
+//                    activeFragment = contactFragment
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
 }
