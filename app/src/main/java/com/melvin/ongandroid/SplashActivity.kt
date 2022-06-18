6131c9a2f982e7fa18d06bd726d40be4ade96f1a
Cami <jcamilouc1@gmail.com>
2022-06-18 16:34:58 -0500

    add timer with kotlin coroutines (splash screen)

--- app/src/main/java/com/melvin/ongandroid/SplashActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/SplashActivity.kt
@@ -10,55 +10,54 @@ import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.Toast
 import com.melvin.ongandroid.databinding.ActivitySplashActivityBinding
 import com.melvin.ongandroid.model.ONGAndroidApp
 import com.melvin.ongandroid.view.LoginActivity
 import com.melvin.ongandroid.view.MainActivity
+import kotlinx.coroutines.*
+import java.lang.StrictMath.log
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashActivityBinding
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Agregar animaciones
-
+        /* This is used to hide the status bar and make
+         the splash screen as a full screen activity.*/
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        // Add animations
         val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo)
         binding.somosMasSplash.startAnimation(bottomAnimation)
 
         val headAnimation = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
         binding.SplashScreenImage.startAnimation(headAnimation)
 
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        //val backgroundImage = findViewById<ImageView>(R.id.SplashScreenImage)
-//        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
-//        binding.SplashScreenImage.startAnimation(slideAnimation)
-
-
-//        Thread.sleep(5000)
-
-
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler().postDelayed({
+        activityScope.launch {
+            delay(5000)
+            Toast.makeText(applicationContext, "Timer has finished", Toast.LENGTH_SHORT).show()
             val token = ONGAndroidApp.prefs.getToken()
             Log.i("SplashScreen", token)
 
             if (token.isNotEmpty()) {
-                startActivity(Intent(this, MainActivity::class.java))
-            } else startActivity(Intent(this, LoginActivity::class.java))
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+            } else startActivity(Intent(applicationContext, LoginActivity::class.java))
 
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
-
+        }
+    }
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
     }
 }
+
